4ad10c6b02ffbaf88df5c845840f34e18958c668
Fabian <draizfabian@gmail.com>
2021-01-03 21:02:25 +0200

    saved csv

--- explore_nlp.py
+++ explore_nlp.py
@@ -2,11 +2,18 @@ import pandas as pd
 import string
 import re
 import nltk
-import os
+import glob, os
 stopwords=nltk.corpus.stopwords.words('english')
+all_files = glob.glob(os.path.join('datasets/Combination', "*.txt"))     # advisable to use os.path.join as this makes concatenation OS independent
 
+df_from_each_file = (pd.read_csv(f,sep='\t',header=None) for f in all_files)
+concatenated_df   = pd.concat(df_from_each_file, ignore_index=True)
+concatenated_df.columns=['body_text','label']
+print(concatenated_df.head())
 
+concatenated_df.to_csv(r'datasets/Combination/multimedia.csv', index = False)
 
+"""
 all_files=[]
 for dirname, _, filenames in os.walk('datasets/Combination'):
     for filename in filenames:
@@ -17,9 +24,12 @@ for file in all_files:
     df=pd.read_csv(os.path.join('datasets/Combination',file),sep='\t',header=None)
     all_df.append(df)
 
-print(all_df)
-"""
-df=pd.read_csv("datasets/Combination/yelp_labelled.txt",sep='\t',header=None)
+for i in range(0,len(all_df)):
+    if i>0:
+        df=pd.concat(all_df[i-1],all_df[i])
+
+
+df=pd.read_csv("datasets/Combination/amazon_cells_labelled.txt",sep='\t',header=None)
 df.columns=['body_text','label']
 print(df.head())
 def clean_text(text):
@@ -28,4 +38,5 @@ def clean_text(text):
     text=[word for word in tokens if word not in stopwords]
     return text
 df["body_text_nonstop"]=df['body_text'].apply(lambda x: clean_text(x.lower()))
-print(df.head())"""
+print(df.head())
+"""
