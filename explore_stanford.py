3708a76bb89f1609fd90c2cf43f56bbcf79b2a88
Fabian <draizfabian@gmail.com>
2021-01-04 15:41:10 +0200

    created label column with labels negative,positive,neutral, saved to csv and created pie chart

--- explore_stanford.py
+++ explore_stanford.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 def create_label(sent_val):
-    if sent_val>=0.5:
+    if sent_val>=0.6:
         return 1
+    elif sent_val<0.6 and sent_val>=0.4:
+        return 0.5
     else:
         return 0
 df=pd.read_csv("datasets/stanfordSentimentTreebank/dictionary.txt",sep='|')
@@ -21,4 +23,28 @@ print(df3.head(5))
 print(df3['sentiment values'].max())
 print(df3['sentiment values'].min())
 df3['label']=df3['sentiment values'].apply(lambda x:create_label(x))
-print(df3.head(5))
+print("Dataframe shape",df3.shape)
+#(239231, 4)
+print("Number of samples",df3.label.value_counts())
+""" for 2 labels
+ 1    151412
+0     87819
+"""
+""" for 3 labels
+0.5    119448
+1.0     65403
+0.0     54380
+"""
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'positive','neutral','negative'
+sizes = [65403,119448,54380]
+explode = (0,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+plt.show()
+df3.to_csv(r'datasets/stanfordSentimentTreebank/Stanford_recreated.csv', index = False)
