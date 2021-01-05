39661cf31758813abdf7be945c770fca385038c2
Fabian <draizfabian@gmail.com>
2021-01-05 18:33:01 +0200

    trying lemmatizing + countvect

--- rf_combination_countvect.py
+++ rf_combination_countvect.py
@@ -7,12 +7,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 df=pd.read_csv("datasets/Combination/combination.csv")
 print(df.head(5))
-ps=nltk.PorterStemmer()
+#ps=nltk.PorterStemmer()
+wn=nltk.WordNetLemmatizer()
 stopwords=nltk.corpus.stopwords.words('english')
 def clean_text(text):
     text=''.join([char for char in text if char not in string.punctuation])
     tokens=re.split('\W+',text)
-    text=[ps.stem(word) for word in tokens if word not in stopwords]
+    text=[wn.lemmatize(word) for word in tokens if word not in stopwords]
     return text
 
 from sklearn.feature_extraction.text import CountVectorizer
@@ -35,14 +36,25 @@ y_pred=rf_model.predict(X_test_count)
 precision,recall,fscore,support=score(y_test,y_pred,pos_label=1,average="binary")
 print("precision={},recall={},accuracy={}".format(round(precision,3),round(recall,3),round((y_pred==y_test).sum()/len(y_test),3)))
 """
+stemming
+n_estimators=50,max_depth=20
 FEATURE IMPORTANCE [(0.06695298628623501, 1728), (0.043137360198200365, 418), (0.03893031817218898, 2274), (0.030679850095938526, 1698), (0.026339039536004805, 1387), (0.021229975155907393, 4114), (0.0190946334228831, 2859), (0.01705063820820016, 4225), (0.017032145694018275, 2583), (0.016900318166265464, 2903)]
 precision=0.73,recall=0.833,accuracy=0.773
 """
 """
+stemming
+n_estimators=150,max_depth=None
 FEATURE IMPORTANCE [(0.047104468261371366, 1728), (0.021415349481218537, 1698), (0.0204382582829047, 2274), (0.015495684802355155, 418), (0.01369200651937918, 1387), (0.010846805822180897, 1144), (0.009084474057207375, 2550), (0.00858436744765013, 115), (0.007887952074728045, 275), (0.007474314252670187, 401)]
 precision=0.845,recall=0.707,accuracy=0.793
 """
 """
+stemming
+n_estimators=150,max_depth=None ,correct count vectorization
 FEATURE IMPORTANCE [(0.006252591103621767, 'body_text')]
 precision=0.798,recall=0.792,accuracy=0.804
+"""
+"""
+lemmatize
+n_estimators=150,max_depth=None ,correct count vectorization
+precision=0.815,recall=0.645,accuracy=0.751
 """
