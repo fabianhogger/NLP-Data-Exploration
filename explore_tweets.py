a7043f8f089ab1964fbc44d3e6de9683b42f1cf2
Fabian <draizfabian@gmail.com>
2021-01-06 20:56:06 +0200

    make cleaner file to later work on

--- explore_tweets.py
+++ explore_tweets.py
@@ -2,15 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #Exploring airline tweets
 df=pd.read_csv("datasets/Usairline/Tweets.csv")
-print(df.head(5))
-print(df.shape)
+#print(df.head(5))
+#print(df.shape)
 #shape  14640 rows 15 columns
-print(df[df['airline_sentiment']=='neutral'].count())
+#print(df[df['airline_sentiment']=='neutral'].count())
 #negative 9178
 #positive 2363
 #neutral 3099
 #random row
-print(df['text'][30],df['airline_sentiment'][30])
+"""print(df['text'][30],df['airline_sentiment'][30])
 print(df['tweet_coord'][30])
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'positive','negative','neutral'
@@ -22,4 +22,14 @@ ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.show()
+plt.show()"""
+def make_label(word):
+    if word== "positive":
+        return 1
+    elif word== "neutral":
+        return 0.5
+    elif word== "negative":
+        return 0
+df=df[['text','airline_sentiment']]
+df['label']=df['airline_sentiment'].apply(lambda x:make_label(x))
+print(df.head(5))
